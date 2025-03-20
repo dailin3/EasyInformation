f6ec94a98b0ba01a3d46bd8c987637d267802cd9
dailin <2687008066@qq.com>
2025-03-20 23:09:34 +0800

    不知啥原因无法使用api登入云邮了，所以只能用这个方法了

--- api/v1/schemas.py
+++ api/v1/schemas.py
@@ -1,19 +1,52 @@
-from flask import url_for, current_app
+from flask import url_for, abort
 import config
 
 class Query:
     pass
 
-def info_schemas():
-    return
+def info_schema(info):
+    return {
+        'id': info.id,
+        'author': info.author,
+        'content': info.content,
+        'crawler_name': info.crawler_name,
+        'crawler_time': info.crawler_time,
+        'discription': info.discription,
+        'href': info.href,
+        'importance': info.importance,
+        'is_deleted': info.is_deleted,
+        'is_read': info.is_read,
+        'group': info.group,
+        'summary': info.summary,
+        'time': info.time,
+        'title': info.title,
+        'type': info.type
+    }
 
-def info_schema():
-    return
+
+def infos_schema(items, current, prev, next, pagination):
+    return {
+        'self': current,
+        'prev': prev,
+        'next': next,
+        'kind': 'infoCollection',
+        'count': pagination["total"],
+        'page': pagination["page"],
+        'pages': pagination["pages"],
+        'per_page': pagination["per_page"],
+        'first': url_for('.infos', page=1, per_page=pagination["per_page"], _external=True),
+        'last': url_for('.infos', page=pagination["pages"], per_page=pagination["per_page"], _external=True),
+        'items': [info_schema(item) for item in items]
+    }
 
 def args_schema(args):
+    allowed_args = {'page', 'per_page', 'group', 'type', 'crawler_name', 'order_by'}
+    if not set(args.keys()).issubset(allowed_args):
+        abort(400, description=f"Unexpected parameter")
+
     query = Query()
-    query.batch = args.get('batch', 1, type=int)
-    query.batch_size = args.get('batch_size', config.DEFAULT_BATCH_SIZE, type=int)
+    query.page = args.get('page', 1, type=int)
+    query.per_page = args.get('per_page', config.DEFAULT_BATCH_SIZE, type=int)
 
     query.group = args.get('group', 'all', type=str)
     query.type = args.get('type', "text/json", type=str)
--- api/v1/source.py
+++ api/v1/source.py
@@ -1,14 +1,17 @@
-from .schemas import info_schema, info_schemas, args_schema
+from .schemas import infos_schema, args_schema
 from api.v1 import api_bp
 import click
 
 from db.db_model import Assignments, Info, Notifications
 from flask import request, jsonify
+from math import ceil
 from flask.views import MethodView
+from flask import url_for
+from flask import abort
 
 api_bp.route('/')(lambda: 'Hello World!')
 
-class InfoAPI(MethodView):
+class InfosAPI(MethodView):
     def get(self):
         query = args_schema(request.args)
         # 创建基础查询
@@ -32,13 +35,47 @@ class InfoAPI(MethodView):
         else:
             peewee_query = peewee_query.order_by(Info.crawler_time.desc())
 
-        # 添加分页条件
-        peewee_query = peewee_query.paginate(query.batch, query.batch_size)
+        # 获取总记录数
+        total_records = peewee_query.count()
 
-        for info in peewee_query:
-            click.echo(info.title)
+        # 分页
+        page = query.page
+        per_page = query.per_page
+        peewee_query = peewee_query.paginate(page, per_page)
 
-        return "",200
+        # 如果没有记录，报错
+        if total_records == 0 or page > ceil(total_records / per_page):
+            abort(404)
+            
+
+        # 计算分页元信息
+        pagination = {
+            'total': total_records,
+            'page': page,
+            'per_page': per_page,
+            'pages': ceil(total_records / per_page) if per_page > 0 else 0
+        }
+
+        # 获取当前页的记录
+        items = list(peewee_query)
+
+        # 构造分页链接
+        current = url_for('.infos', batch=page, batch_size=per_page, _external=True)
+        prev = url_for('.infos', batch=page - 1, batch_size=per_page, _external=True) if page > 1 else None
+        next = url_for('.infos', batch=page + 1, batch_size=per_page, _external=True) if page < pagination['pages'] else None
+
+        # 返回分页结果
+        return jsonify(infos_schema(items, current, prev, next, pagination))
+    
+class Info(MethodView):
+    def delete(self, id):
+        info = Info.get_or_none(Info.id == id)
+        if info is None:
+            abort(404)
+        info.is_deleted = True
+        info.save()
+        return '', 204
     
 
-api_bp.add_url_rule('/infos', view_func=InfoAPI.as_view('infos'), methods=['GET'])
+api_bp.add_url_rule('/infos', view_func=InfosAPI.as_view('infos'), methods=['GET'])
+api_bp.add_url_rule('/info/<int:id>', view_func=Info.as_view('info'), methods=['DELETE'])
