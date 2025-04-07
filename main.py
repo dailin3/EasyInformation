c2bb9fe6a3e251e08014f97416ce3e457588fe06
dailin <2687008066@qq.com>
2025-04-07 22:23:57 +0800

    do a little change

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ from api.v1 import api_bp
 from flask import Flask
 from subthread import *
 
-# Start the crawlers threads
-start_crawlers_threads()
+# judge if the script is first runing
+if __name__ == "__main__":
 
-# Start the notification thread
-notification_thread = NotificationThread()
-notification_thread.start_loop()
+    first_run = False
 
-# Start the flask app
-app = Flask(__name__)
-app.register_blueprint(api_bp, url_prefix='/api/v1')
+    if not os.path.exists(os.path.join(os.path.join(config.CWD, "db"),"main.db")):
+        # create the database
+        database.connect()
+        database.create_tables([Info, Assignments,Notifications])
+        database.close()
+        print("Database created successfully.")
+        first_run = True
 
-app.route('/')(lambda: 'Hello World!')
+    # Start the crawlers threads
+    start_crawlers_threads()
 
-app.run(debug=True)
+    # Start the notification thread
+    notification_thread = NotificationThread()
+    notification_thread.start_loop()
+
+    # Start the flask app
+    app = Flask(__name__)
+    app.register_blueprint(api_bp, url_prefix='/api/v1')
+
+    app.route('/')(lambda: 'Hello World!')
+
+    app.run(debug=True)
